eeb0c9219eb184e78c3f476439d3457d64e94459
Kvazar103 <nazar.voinarovych250@gmail.com>
2022-09-26 23:38:45 +0300

    hw3task1 fix

--- src/components/Users.js
+++ src/components/Users.js
@@ -6,15 +6,11 @@ import Post from "./Post";
 export function Users(){
 
     let [id,setId]=useState(null)
-    let [user,setUser]=useState(null)
     let [users,setUsers]=useState([])
     let [posts,setPosts]=useState([])
 
     const idLift=(id)=>{
         setId(id)
-    }
-    const lift = (obj) => {
-      setUser(obj)
     }
 
     useEffect(()=>{
@@ -25,7 +21,7 @@ export function Users(){
     },[id])
 
     return(<div>
-        {users.map((users ,index)=>(<User users={users} key={index} lift={lift} idLift={idLift}/>))}
+        {users.map((users ,index)=>(<User users={users} key={index} idLift={idLift}/>))}
         {posts.map((posts,index)=>(<Post posts={posts} key={index}/>))}
     </div>)
 }
