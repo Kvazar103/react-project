522bba4216c91ac2857afc710b9a02b724a62950
Kvazar103 <nazar.voinarovych250@gmail.com>
2022-10-19 12:10:03 +0300

    tp

--- app.js
+++ app.js
@@ -3,3 +3,16 @@ var user = {
     age: 18,
     gender: "male"
 };
+function sum(a, b) {
+    return a + b;
+}
+function showSum(a, b) {
+    console.log(a + b);
+}
+function incAge(someUser, inc) {
+    someUser.age += inc;
+    return someUser;
+}
+console.log(sum(1, 2));
+showSum(2, 3);
+incAge(user, 2);
--- app.ts
+++ app.ts
@@ -11,3 +11,21 @@ let user:IUser={
     gender:"male"
 }
 
+function sum(a: number, b: number): number {
+    return a + b;
+}
+
+function showSum(a:number,b:number){
+    console.log(a+b)
+}
+
+function incAge(someUser:IUser,inc:number):object{
+    someUser.age+=inc
+    return someUser
+}
+
+console.log(sum(1,2))
+showSum(2,3)
+incAge(user,2)
+
+
