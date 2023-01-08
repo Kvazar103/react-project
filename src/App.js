4e79e3d622b738fee51dbff60d4495e7786b08fb
Kvazar103 <nazar.voinarovych250@gmail.com>
2023-01-08 17:57:00 +0200

    init

--- src/App.js
+++ src/App.js
@@ -1,23 +1,38 @@
-import logo from './logo.svg';
 import './App.css';
+import {w3cwebsocket as W3CWebSocket} from "websocket";
+import {useEffect, useState} from "react";
 
+const url='ws://localhost:8080/chat';  //тут ми кажемо що ми в майбутньому будем підписуватися на цю урлу
+const client=new W3CWebSocket(url); //це клієнт який на цю урлу підписується
 function App() {
+
+  let [msgs,setMsgs]=useState([]);
+
+  useEffect(()=>{
+    client.onopen=(e)=>{
+      console.log("connected",e)
+    };
+    client.onmessage=(e)=>{
+      console.log(e.data);
+      msgs.push(e.data);
+      setMsgs([...msgs]);
+    }
+  },[])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <form onSubmit={(e)=>{
+        e.preventDefault();
+       let messageToSend= e.target.message.value;//звертаємося до нашої форми його поля message і зчитуєм його value
+        console.log(messageToSend);
+        client.send(messageToSend);  //звертаємося до нашого клієнта який вже связаний з чим чатом і кажемо сенд
+      }}>
+        <input type="text" name={'message'}/>
+        <button>send msg</button>
+      </form>
+      {
+        msgs.map(value => <div>{value}</div>)
+      }
     </div>
   );
 }
